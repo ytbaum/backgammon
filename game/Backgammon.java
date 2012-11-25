dd1dbdc0bb69282e845ba0ce8fce7611eb91da34
yoni <ytbaum@gmail.com>
2012-11-24 21:37:18 -0500

    added 'getAvailableMoves' function, to get all available moves as an array

--- game/Backgammon.java
+++ game/Backgammon.java
@@ -1,12 +1,15 @@
 package game;
 
 import java.util.Random;
+import java.util.Arrays;
 import board.*;
 
 public class Backgammon
 {
 
     private int[] dice = {0, 0};
+
+    public static final int DIE_NUM_SIDES = 6;
 
     public static void main(String[] args)
     {
@@ -19,6 +22,9 @@ public class Backgammon
         this.rollDice();
         System.out.println(this.getDiceString());
 
+        int[] availableMoves = this.getAvailableMoves();
+        System.out.println(Arrays.toString(availableMoves));
+
         return;
     }
 
@@ -26,7 +32,7 @@ public class Backgammon
     {
         Random rand = new Random();
         for (int i = 0; i < this.dice.length; i++) {
-            this.dice[i] = rand.nextInt(6) + 1;
+            this.dice[i] = rand.nextInt(DIE_NUM_SIDES) + 1;
         }
 
         return;
@@ -37,6 +43,28 @@ public class Backgammon
         String output = Integer.toString(this.dice[0]) + " " + Integer.toString(this.dice[1]);
 
         return output;
+    }
+
+    private int[] getAvailableMoves()
+    {
+        int[] availableMoves = new int[DIE_NUM_SIDES];
+
+        // dice are 1-indexed; convert them to 0-indexed
+        int[] zDice = new int[2];
+        zDice[0] = this.dice[0] - 1;
+        zDice[1] = this.dice[1] - 1;
+
+        for (int i = 0; i < DIE_NUM_SIDES; i++) {
+            if (i == zDice[0] && i == zDice[1]) {
+                availableMoves[i] = 4;
+            } else if (i == zDice[0] || i == zDice[1]) {
+                availableMoves[i] = 1;
+            } else {
+                availableMoves[i] = 0;
+            }
+        }
+
+        return availableMoves;
     }
 }
 
